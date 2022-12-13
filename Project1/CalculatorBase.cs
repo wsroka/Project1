798864936cec38a1864834a08ea9166ae4952261
Wiktoria <wiktoria123sr@gmail.com>
2022-12-13 22:36:22 +0100

    improving the CalculatorBase class

---
+++ Project1/CalculatorBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Project1
+{
+    internal class CalculatorBase
+    {
+        public int GetAdd(int value1, int value2) => value1 + value2;
+        public int GetSubtract(int value1, int value2) => value1 - value2;
+        public int GetMultiply(int value1, int value2) => value1 * value2;
+        public int GetDivide(int value1, int value2) => value1 / value2;
+    }
+}
