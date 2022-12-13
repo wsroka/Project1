6c93652d36c0aec801fdd13246cbd2ea9fa55be7
Wiktoria <wiktoria123sr@gmail.com>
2022-12-13 21:22:05 +0100

    Add simple calculator functionals

---
+++ Project1/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Calculator
+    {
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+        public int Subtract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+        public int Divide(int value1, int value2)
+        {
+            return value1 / value2;
+        }
+    }
+}
