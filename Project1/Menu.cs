ebffe71e8f038ea840faf1080c593f925843a5ad
Wiktoria <wiktoria123sr@gmail.com>
2022-12-15 06:40:10 +0100

    Add class Menu and constructor this class.

---
+++ Project1/Menu.cs
@@ -0,0 +1,22 @@
+﻿namespace Project1
+{
+    internal class Menu
+    {
+        public Menu()
+        {
+            Console.WriteLine("1. Dodawanie");
+            Console.WriteLine("2. Odejmowanie");
+            Console.WriteLine("3. Mnożenie");
+            Console.WriteLine("4. Dzielenie");
+            Console.WriteLine("5. Liczba pierwsza");
+            Console.WriteLine("6. Silnia");
+            Console.WriteLine("7. Liczba doskonała");
+            Console.WriteLine("8. Średnia liczba elementów tablicy");
+            Console.WriteLine("9. Największa element tablicy");
+            Console.WriteLine("10. Najmniejszy element tablicy");
+            Console.WriteLine("11. Zapisz tekst do pliku txt");
+            Console.WriteLine("12. Wyświetl tekst z pliku txt");
+            Console.WriteLine("13. Wyświetl konkretną linie z pliku txt");
+        }
+    }
+}
