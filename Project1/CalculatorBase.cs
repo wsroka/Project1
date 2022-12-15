6af64fc349a98ee5a8aef64db3e26f88619ee725
Wiktoria <wiktoria123sr@gmail.com>
2022-12-15 20:32:00 +0100

    Change name

--- Project1/CalculatorBase.cs
+++ Project1/CalculatorBase.cs
@@ -2,9 +2,9 @@
 {
     internal class CalculatorBase
     {
-        public int GetAdd(int value1, int value2) => value1 + value2;
-        public int GetSubtract(int value1, int value2) => value1 - value2;
-        public int GetMultiply(int value1, int value2) => value1 * value2;
-        public int GetDivide(int value1, int value2) => value1 / value2;
+        public int ExecuteAddition(int value1, int value2) => value1 + value2;
+        public int ExecuteSubtraction(int value1, int value2) => value1 - value2;
+        public int ExecuteMultiplication(int value1, int value2) => value1 * value2;
+        public int ExecuteDivision(int value1, int value2) => value1 / value2;
     }
 }
