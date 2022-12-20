922e5cd7be13764eb0789588afeb1f2277ae0a12
Wiktoria <wiktoria123sr@gmail.com>
2022-12-20 20:53:17 +0100

    Changes in program.cs

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,49 +6,46 @@ internal class Program
     {
         Menu menu = new Menu();
         menu.ShowMainMenu();
-        Console.Write("\nWybierz numer z manu głównego: ");
-        int numberMainMenu = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\nWybierz numer z menu głównego: ");
+        int baseOption = Convert.ToInt32(Console.ReadLine());
 
-        int value1;
-        int value2;
-        if (numberMainMenu == 1)
+        if (baseOption == 1)
         {
-            Console.WriteLine();
             CalculatorBase calculatorbase = new CalculatorBase();
             menu.ShowBaseCalculatorMenu();
             Console.Write("\nWybierz numer z menu prostego kalkulatora: ");
-            int result = Convert.ToInt32(Console.ReadLine());
+            int baseCalculatorOption = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
-            if (result == 1)
+            if (baseCalculatorOption == 1)
             {
                 Console.Write("Podaj pierwszą liczbę: ");
-                value1 = Convert.ToInt32(Console.ReadLine());
+                int value1 = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Podaj drugą liczbę: ");
-                value2 = Convert.ToInt32(Console.ReadLine());
+                int value2 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Wynik dodawania: " + calculatorbase.ExecuteAddition(value1, value2));
             }
-            else if (result == 2)
+            else if (baseCalculatorOption == 2)
             {
                 Console.Write("Podaj pierwszą liczbę: ");
-                value1 = Convert.ToInt32(Console.ReadLine());
+                int value1 = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Podaj drugą liczbę: ");
-                value2 = Convert.ToInt32(Console.ReadLine());
+                int value2 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Wynik odejmowania: " + calculatorbase.ExecuteSubtraction(value1, value2));
             }
-            else if (result == 3)
+            else if (baseCalculatorOption == 3)
             {
                 Console.Write("Podaj pierwszą liczbę: ");
-                value1 = Convert.ToInt32(Console.ReadLine());
+                int value1 = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Podaj drugą liczbę: ");
-                value2 = Convert.ToInt32(Console.ReadLine());
+                int value2 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Wynik mnożenia: " + calculatorbase.ExecuteMultiplication(value1, value2));
             }
-            else if (result == 4)
+            else if (baseCalculatorOption == 4)
             {
                 Console.Write("Podaj pierwszą liczbę: ");
-                value1 = Convert.ToInt32(Console.ReadLine());
+                int value1 = Convert.ToInt32(Console.ReadLine());
                 Console.Write("Podaj drugą liczbę: ");
-                value2 = Convert.ToInt32(Console.ReadLine());
+                int value2 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Wynik dzielenia: " + calculatorbase.ExecuteDivision(value1, value2));
             }
         }
