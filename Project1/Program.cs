7f8dd7a8be08b570d18c2bad040ea831434dcd54
Wiktoria <wiktoria123sr@gmail.com>
2023-01-10 21:08:30 +0100

    Add operations on txt files

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,9 +1,12 @@
 ﻿using Project1;
+using System;
+using System.IO;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        ConsoleColors consoleColors = new ConsoleColors();
         Menu menu = new Menu();
         menu.ShowMainMenu();
         Console.Write("\nWybierz numer z menu głównego: ");
@@ -18,11 +21,11 @@ internal class Program
             Console.WriteLine();
             if (baseCalculatorOption == 1)
             {
-                Console.Write("Podaj pierwszą liczbę: ");
+                consoleColors.ColorObject("Podaj pierwszą liczbę: ", ConsoleColor.Green);
                 int value1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Podaj drugą liczbę: ");
+                consoleColors.ColorObject("Podaj drugą liczbę: ", ConsoleColor.Magenta);
                 int value2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Wynik dodawania: " + calculatorbase.ExecuteAddition(value1, value2));
+                consoleColors.ColorObject("Wynik dodawania: " + calculatorbase.ExecuteAddition(value1, value2), ConsoleColor.DarkRed);
             }
             else if (baseCalculatorOption == 2)
             {
@@ -42,11 +45,47 @@ internal class Program
             }
             else if (baseCalculatorOption == 4)
             {
-                Console.Write("Podaj pierwszą liczbę: ");
-                int value1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Podaj drugą liczbę: ");
-                int value2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Wynik dzielenia: " + calculatorbase.ExecuteDivision(value1, value2));
+                while (true)
+                {
+                    Console.Write("Podaj pierwszą liczbę: ");
+                    int value1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Podaj drugą liczbę: ");
+                    int value2 = Convert.ToInt32(Console.ReadLine());
+                }
+                try
+                {
+
+                }
+                catch (DivideByZeroException e)
+                {
+
+                    throw;
+                }
+            }
+        }
+        if (baseOption == 3)
+        {
+            TxtFile txtFile = new TxtFile();
+            menu.ShowTxtFilesMenu();
+            Console.Write("\nWybierz numer z menu: ");
+            int actionOnTxtFile = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            if (actionOnTxtFile == 11)
+            {
+                Console.WriteLine("Podaj tekst: ");
+                string text = Console.ReadLine();
+                txtFile.SaveText(text);
+            }
+            else if (actionOnTxtFile == 12)
+            {
+                Console.WriteLine("\nZawartośc pliku: ");
+                txtFile.ViewTheFile();
+            }
+            else if (actionOnTxtFile == 13)
+            {
+                Console.WriteLine("Podaj, którą linie tekstu wyświetlić: ");
+                int txtline = Convert.ToInt32(Console.ReadLine());
+                txtFile.ViewTheLine(txtline);
             }
         }
     }
